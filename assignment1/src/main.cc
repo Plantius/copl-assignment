19316d6c009d0f6b5357f47830d254c8e02215f1
niels <niels>
2023-09-13 12:34:06 +0200

    Added the print_cout function to standard.h, which prints a const char* to the standard output

---
+++ assignment1/src/main.cc
@@ -0,0 +1,9 @@
+#include "../include/standard.h"
+using namespace std;
+
+int main(){
+    const char* input = "aa";
+    print_cout(input);
+
+    return 0;
+}
---
+++ assignment1/src/standard.cc
@@ -0,0 +1,6 @@
+#include "../include/standard.h"
+using namespace std;
+
+void print_cout(const char* input){
+    cout << input << endl;
+}
