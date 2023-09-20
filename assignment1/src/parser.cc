ccd1e86a17fe07c2afea6e382e8fd02bf9bce50c
Lana <nogmeerlana@gmail.com>
2023-09-20 15:03:23 +0200

    begin aan tokenvalid voor het idee

--- assignment1/src/parser.cc
+++ assignment1/src/parser.cc
@@ -67,7 +67,6 @@ void tokenSwitch(const char inputChar, tokenList* tList){
 
 
 bool tokenValid(tokenList* tList){
-
     // RIGHT OF SYMBOL:
     // LPAR: only VAR, LPAR, SPACE
     // RPAR: only VAR, LPAR, RPAR, SPACE
@@ -76,11 +75,40 @@ bool tokenValid(tokenList* tList){
     // LPAR: only VAR, LPAR, RPAR, SPACE
     // RPAR: only VAR, SPACE, RPAR
 
-    // cannot start with RPAR
-    
+    // First we're going to count the amount of parantheses
+    int leftParCounter = 0;
+    int rightParCounter = 0;
+
+    int size = tList -> getLength();
+    for (int i = 0; i < size; i++){
+        int tempToken = tList -> getToken(i); // check ik later wel ff
+        if (tempToken == lpar){
+            leftParCounter ++;
+        }
+        if ( tempToken == rpar){
+            rightParCounter ++;
+        }
+    }
+
+    if (leftParCounter!=rightParCounter){
+        cout << "The token is invalid: not enough beginning/closing parantheses." << endl;
+        return false;
+    }
+
+    // Now we're checking whether the parantheses contain an expression or variable
+    for (int j = 0; j < size; j++){
+        int tempToken = tList -> getToken(j); // check ik later wel ff
+        if (tempToken == rpar){
+            int temp2 = tList -> getToken(j-1);
+            if (temp2 == lpar){
+                cout << "No variable or expression in parantheses." << endl;
+                return false;
+            }
+        }
+    }
     
 
-    
+    return true;    
 }
 
 
