a1d9136c4efa119dc4c434c91a5421688b8f6462
niels <niels>
2023-10-16 16:39:14 +0200

    Added debug mode

--- assignment1/src/main.cc
+++ assignment1/src/main.cc
@@ -13,25 +13,43 @@
 using namespace std;
 
 // Tokenizes a given string, and parses it
-int main(int argc, char** argv){
+int main(int argc, char* argv[]){
     clock_t t1, t2;
     parser* parse = new parser; 
     string input = emptyStr, filepath = "NULL";
+    bool debug = false;
+    int nr = 0, avg = 0;
     
     if(argc < 2){
         delete parse;
         return 2;
+    }else if (string(argv[1]) == "d"){
+        debug = true;
     }
 
     try{
-        filepath = string(argv[1]);
-        input = readFile(filepath);
-        validInput(input);
-        t1 = clock();
-        // Checks if any errors are thrown from the stringTokenizer function
-        parse->stringTokenizer(input);
-        t2 = clock();
-        cout << "Tokenizer " << (((double)(t2-t1))/CLOCKS_PER_SEC) << " in " << (t2-t1) << " ticks"<< endl; 
+        if (!debug){
+            filepath = string(argv[1]);
+            input = readFile(filepath);
+            validInput(input);
+            t1 = clock();
+            // Checks if any errors are thrown from the stringTokenizer function
+            parse->stringTokenizer(input);
+            t2 = clock();
+            cout << "Tokenizer " << (((double)(t2-t1))/CLOCKS_PER_SEC) << " in " << (t2-t1) << " ticks"<< endl; 
+        }else {
+            while (getline(cin >> ws, input)){
+                t1 = clock();
+                // Checks if any errors are thrown from the stringTokenizer function
+                parse->stringTokenizer(input);
+                t2 = clock();
+                cout << "Tokenizer " << (((double)(t2-t1))/CLOCKS_PER_SEC) << " in " << (t2-t1) << " ticks"<< endl; 
+                input.clear();
+                nr++;
+                avg +=(t2-t1);
+            }
+            cout << "Average " <<(((double)(avg)/nr)/CLOCKS_PER_SEC) << " in " << (avg)/nr << " ticks"<< endl; 
+        }
     }
     catch(memoryError & error){
         printError<memoryError>(error, filepath);
