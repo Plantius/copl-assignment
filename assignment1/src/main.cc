8c88f7a55094bf9bfcfd0925ce46cbd2dc9fa92f
niels <niels>
2023-10-18 11:25:48 +0200

    Main reduced in size

--- assignment1/src/main.cc
+++ assignment1/src/main.cc
@@ -18,17 +18,13 @@ int main(int argc, char* argv[]){
     clock_t t1, t2;
     parser* parse = new parser; 
     string input = emptyStr, filepath = "NULL";
-    bool debug = false;
     
     if(argc < 2){
         delete parse;
         return 2;
-    }else if (string(argv[1]) == "d"){
-        debug = true;
     }
 
     try{
-        if (!debug){
             filepath = string(argv[1]);
             ifstream file(filepath);
             if(!file.good() || !file.is_open()){
@@ -43,23 +39,6 @@ int main(int argc, char* argv[]){
                 t2 = clock();
                 cout << "Tokenizer " << (((double)(t2-t1))/CLOCKS_PER_SEC) << " in " << (t2-t1) << " ticks"<< endl; 
             }
-            
-            t1 = clock();
-            // Checks if any errors are thrown from the stringTokenizer function
-            parse->stringTokenizer(input);
-            t2 = clock();
-            cout << "Tokenizer " << (((double)(t2-t1))/CLOCKS_PER_SEC) << " in " << (t2-t1) << " ticks"<< endl; 
-        }else {
-            while (getline(cin >> ws, input)){
-                validInput(input);
-                t1 = clock();
-                // Checks if any errors are thrown from the stringTokenizer function
-                parse->stringTokenizer(input);
-                t2 = clock();
-                cout << "Tokenizer " << (((double)(t2-t1))/CLOCKS_PER_SEC) << " in " << (t2-t1) << " ticks"<< endl; 
-                input.clear();
-            }
-        }
     }
     catch(memoryError & error){
         printError<memoryError>(error, filepath);
