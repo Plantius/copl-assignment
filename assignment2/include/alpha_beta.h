7e589af1a5d15f48efd5cb89943de21e207cab1d
Lana <nogmeerlana@gmail.com>
2023-11-15 12:45:16 +0100

    Beginning with the alpha and beta operations

---
+++ assignment2/include/alpha_beta.h
@@ -0,0 +1,29 @@
+/**
+* Declaration of the alpha concersion and beta reduction
+* Niels Versteeg (s3322637)
+* Lana van Sprang (s3272192)
+* alpha_beta.h
+* 15-11-2023
+**/
+
+#ifndef ALPHA_BETA_H
+#define ALPHA_BETA_H    
+#include <iostream>
+#include "../include/tree.h"
+
+class alpha_beta{
+    public:
+        alpha_beta();
+        bool performConversion(node* begin);
+        bool performReduction(node* begin);
+        bool needsAlpha(node* begin, tokenList & L);
+        bool needsBeta(node* begin, tokenList & L);
+        void makeAbstract(node* begin, tokenList & L);
+
+    private:
+        tree T;
+
+
+};
+
+#endif
---
+++ assignment2/src/alpha_beta.cc
@@ -0,0 +1,64 @@
+/**
+* Implementation of the alpha conversion 
+  and beta reduction.
+* Niels Versteeg (s3322637)
+* Lana van Sprang (s3272192)
+* alpha_beta.cc
+* 15-11-2023
+**/ 
+
+#include "../include/standard.h"
+#include "../include/parser.h"
+#include "../include/token.h"
+#include "../include/error.h"
+#include "../include/alpha_beta.h"
+#include <fstream>
+using namespace std;
+
+alpha_beta::alpha_beta(){
+
+} // constructor
+
+void alpha_beta::makeAbstract(node* begin,  tokenList & L){
+    bool done = false;
+    if(needsBeta(begin, L)){
+        if(needsAlpha(begin, L)){
+            performConversion(begin);
+        }
+        done = performReduction(begin);
+        if(!done){
+            cout << "There went something wrong." << endl;
+        }
+    }
+
+} // makeAbstract
+
+bool alpha_beta::needsBeta(node* begin, tokenList & L){
+
+    for(int i=0; i < L.getLength(); i++){
+        if (L.getToken(i)->id == LAMBDA){
+
+        }
+    }
+
+    return true;
+} // needsBeta
+
+
+bool alpha_beta::needsAlpha(node* begin, tokenList & L){
+
+    return true;
+
+} // needsAlpha
+
+bool alpha_beta::performReduction(node* begin){
+
+    return true;
+
+} // performReduction
+
+bool alpha_beta::performConversion(node* begin){
+
+    return true;
+    
+} //performConversion
